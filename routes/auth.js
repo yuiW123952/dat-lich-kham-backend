2249ac8413e3c8b909e213b42621f15ef404bfee
quynhnhu39 <tnhu5006@gmail.com>
2026-06-03 10:16:44 +0700

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -8,7 +8,9 @@ const auth    = require('../middleware/auth');
 
 // Cấu hình Gmail transporter
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: 'smtp.gmail.com',
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.GMAIL_USER,
     pass: process.env.GMAIL_PASS,
